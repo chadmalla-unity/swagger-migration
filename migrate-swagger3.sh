6f51944ebc511bcf09b4b2124f1e372a524cc597
Erich Mauerböck <9578115+Enrice@users.noreply.github.com>
2022-04-26 17:54:16 +0200

    modify according to my needs
    add some non-greediness
    parameterized Api becomes Tag

--- migrate-swagger3.sh
+++ migrate-swagger3.sh
@@ -8,7 +8,7 @@ echo ''
 echo "Migrating Swagger v2 to Swagger v3 OpenAPI annotations"
 echo ''
 
-EXPRESSION="s/io\.swagger\.annotations\.Api;/io\.swagger\.v3\.oas\.annotations\.OpenAPIDefinition;/g;\
+EXPRESSION="s/import io\.swagger\.annotations\.Api;/import io\.swagger\.v3\.oas\.annotations\.OpenAPIDefinition;\nimport io\.swagger\.v3\.oas\.annotations\.tags.Tag;/g;\
 s/io\.swagger\.annotations\.ApiOperation;/io\.swagger\.v3\.oas\.annotations\.Operation;/g;\
 s/io\.swagger\.annotations\.ApiParam;/io\.swagger\.v3\.oas\.annotations\.Parameter;/g;\
 s/io\.swagger\.annotations\.ApiResponse;/io\.swagger\.v3\.oas\.annotations\.responses\.ApiResponse;/g;\
@@ -17,11 +17,12 @@ s/io\.swagger\.annotations\.ApiModelProperty;/io\.swagger\.v3\.oas\.annotations\
 s/io\.swagger\.annotations\.ApiModelProperty;/io\.swagger\.v3\.oas\.annotations\.media\.Schema;/g;\
 \
 s/@Api$/@OpenAPIDefinition/g;\
-s/@Api(\"\(.*\)\"/@OpenAPIDefinition(info = @Info(description = \"\1\")/g;\
+s/@Api(\"\([^)]*\)\"/@Tag(name = \"\1\"/g;\
 s/@ApiOperation(/@Operation(summary = /g;\
 s/@ApiResponse(code = \([0-9]\{3\}\), message = \"\(.*\)\", response = \(.*\.class)\)/@ApiResponse(responseCode = \"\1\", description = \"\2\", content = @Content(schema = @Schema(implementation = \3))/g;\
-s/@ApiResponse(code = \([0-9]\{3\}\), message = \"\(.*\)\")/@ApiResponse(responseCode = \"\1\", description = \"\2\")/g;\
-s/@ApiParam(required = \(.*\), value = \"\(.*\)\")/@Parameter(required = \1, description = \"\2\")/g;\
+s/@ApiResponse(code = \([0-9]\{3\}\), message = \"\([^)]*\)\")/@ApiResponse(responseCode = \"\1\", description = \"\2\")/g;\
+s/@ApiParam(required = \(.*\), value = \"\([^)]*\)\")/@Parameter(required = \1, description = \"\2\")/g;\
+s/@ApiParam(value = \"\(.*\)\", required = \([^)]*\))/@Parameter(required = \2, description = \"\1\")/g;\
 s/@ApiModelProperty(\"/@Schema(description = \"/g;\
 s/@ApiModelProperty(notes/@Schema(description/g;\
 s/@ApiModelProperty(value/@Schema(description/g;\
@@ -29,7 +30,7 @@ s/@ApiModelProperty(value/@Schema(description/g;\
 
 for FILE in $FILES
 do
-	CMD="sed -i -e '${EXPRESSION}' '${FILE}'"
+    CMD="sed -i -e '${EXPRESSION}' '${FILE}'"
     echo ''
     echo "Migrating ${FILE} ..."
     eval ${CMD}
